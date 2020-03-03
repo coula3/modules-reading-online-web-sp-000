724db9cf037e528f588eb171efaaba222eea9d51
Alvin Coulson <aaacoulson@gmail.com>
2020-03-03 21:13:06 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -17,9 +17,9 @@ module FancyDance
     end
   end
     
-    module ClassMethods
-      def metadata
-        "This class produces objects that love to dance."
-      end
+  module ClassMethods
+    def metadata
+      "This class produces objects that love to dance."
     end
+  end
 end
